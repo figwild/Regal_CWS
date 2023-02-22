c2a54a5047cfb6b5f2e321757adf1c2bb87c0d9f
FIG Wildlife <126036120+figwild@users.noreply.github.com>
2023-02-22 08:46:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,7 +105,7 @@ IMAGE_TYPES = ["png", "jpg"]
 EXTERNAL_DEPENDENCIES = {
     "Regal_Project_1.pkl": {
         "url": "https://drive.google.com/file/d/1LpppfviJGkmi6z-6g8tXSmfJiqILveGV/view?usp=sharing",
-        "size": 179197877
+        "size": 179197952
     }
 }
 
